9fa30ee32b44222cb30b1ee300bfbfdfeedf7af8
André Filipe Goulart <afgoulart.rj@gmail.com>
2013-09-18 20:13:11 -0300

    Update jquery.gallerybuild.js
    Alterando o script para forma de plugin, tornando-o genérico dando ao usuario liberdade de escolha dos nomes dos seletores.

--- js/jquery.gallerybuild.js
+++ js/jquery.gallerybuild.js
@@ -1,192 +1,266 @@
+var gallerySliders = {};
 (function(){
-	// ======================= imagesLoaded Plugin ===============================
-	// https://github.com/desandro/imagesloaded
+	Gallerybuild = function(opt){
+		if(!opt) var opt = {};
 
-	// $('#my-container').imagesLoaded(myFunction)
-	// execute a callback when all images have loaded.
-	// needed because .load() doesn't work on cached images
+		settings = {
+			classPanelGallery: 'PanelGallery',
+			seletorsPanelGalley: '.infosBottom, .headerTitleGallery, .bx-prev, .bx-next',
+			seletorHeader: '.headerTitleGallery',
+			seletorFooter: '.infosBottom',
+			seletorNext: '.bx-next',
+			seletorPrev: '.bx-prev',
+			seletorGallery: '.box-gallery',
+			seletorGlobalGallery: '#galeria',
+			seletorParentGallery: '.galleryViewPort',
+			seletorChangeGallery: '.changeGallery',
+			seletorChangeGalleryContentLink: '.gallery',
+			keybordControls: true,
+			optionsBxSlider: {
 
-	// callback function gets image collection as argument
-	//  this is the container
-
-	// original: mit license. paul irish. 2010.
-	// contributors: Oren Solomianik, David DeSandro, Yiannis Chatzikonstantinou
-
-	$.fn.imagesLoaded = function( callback ) {
-		var $images = this.find('img'),
-			len = $images.length,
-			_this = this,
-			blank = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
-
-		function triggerCallback() {
-			callback.call( _this, $images );
-		}
-
-		function imgLoaded() {
-			if ( --len <= 0 && this.src !== blank ){
-				setTimeout( triggerCallback );
-				$images.unbind( 'load error', imgLoaded );
 			}
-		}
+		};
 
-		if ( !len ) {
-			triggerCallback();
-		}
+		$.extend(settings, opt);
 
-		$images.bind( 'load error',  imgLoaded ).each( function() {
-			// cached images don't fire load sometimes, so we reset src.
-			if (this.complete || this.complete === undefined){
-				var src = this.src;
-				// webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
-				// data uri bypasses webkit log warning (thx doug jones)
-				this.src = blank;
-				this.src = src;
-			}
+		var $allGalleries = $(settings.seletorGallery);
+		
+		if(!$allGalleries.size()) return false;
+		
+		$allGalleries.css('visibility', 'hidden');
+
+		//TODO :: ajustar esse trecho
+		var $galleryContent = $allGalleries.eq(0);
+
+		$slideContent = $galleryContent.find(".bx-slider");
+
+		$(settings.seletorChangeGallery + " a").on('click.changeGallery',function(e){
+			e.preventDefault();
+			var $link = $(this);
+			var idGallery = $link.data('target');
+			var $currentGallery = $(settings.seletorGallery + ':visible');
+
+			if($currentGallery.attr('id') == idGallery && $currentGallery.size() == 1) return false;
+
+			var $gallery = $('#'+idGallery);
+			$gallery.togglePainels('slow', true);
+
+			$currentGallery.fadeOut(200,function(){
+				$(settings.seletorChangeGallery + " " + settings.seletorChangeGalleryContentLink).removeClass('active');
+
+				if(!$gallery.find('.bx-slider').data('galleryLoaded')){
+					$gallery.css('visibility', 'hidden');
+					$gallery.find('.bx-slider').createSlide(idGallery);
+					$gallery.find(settings.seletorsPanelGalley).data('element-hide',true).addClass(settings.classPanelGallery);
+				}
+
+				$gallery.fadeIn(200, function(){
+					$gallery.css('visibility', 'visible');
+					$link.parent().addClass('active');
+				});
+			});
+
+			return false;
 		});
 
+		if(settings.keybordControls){
+			$(window).on({
+				keyup: function(event){
+					var $currentGallery = $(settings.seletorGallery + ':visible');
+					if(event.which == 37){ //PREV
+						$currentGallery.find('.bx-prev').trigger('click');
+					}
+					if(event.which == 39){ //NEXT
+						$currentGallery.find('.bx-next').trigger('click');
+					}
+					
+					if(event.which == 38){ //PREV GALLERY
+						$currentGallery.changeGalleryKeybord('prev');
+					}
+					
+					if(event.which == 40){ //NEXT GALLERY
+						$currentGallery.changeGalleryKeybord('next');
+					}
+				},
+				keydown: function(){
+
+					if(event.which == 38){ //PREV GALLERY
+						event.preventDefault();
+					}
+					
+					if(event.which == 40){ //NEXT GALLERY
+						event.preventDefault();
+					}	
+				}
+			});
+		}
+
+		$(settings.seletorChangeGallery + " a").first().trigger('click');
+	};
+
+
+
+	$.fn.id = function(){
+		return this.attr('id');
+	};
+
+	$.fn.changeGalleryKeybord = function(direction){
+		var id = this.id();
+		switch(direction){
+			case "prev":
+				$(settings.seletorChangeGallery + ' a[data-target="' + id + '"]').parent().prev().find('a').trigger('click');
+				break;
+			case "next":
+				$(settings.seletorChangeGallery + ' a[data-target="' + id + '"]').parent().next().find('a').trigger('click');
+				break;
+		}
+	};
+
+	$.fn.togglePainels = function(speedAction, force_show, $elPanels){
+		if(!speedAction) speedAction = 1000;
+		if(!force_show) force_show = false;
+		if(!$elPanels) $elPanels = this.find('.'+settings.classPanelGallery);
+	
+		$elPanels.each(function(){
+			var $this = $(this);
+			var direction = $this.data('direction-hide');
+			var elHide = $this.data('element-hide');
+			var height = $this.height();
+			var width = $this.width();
+			var checkCSSTransition = $('html').hasClass('csstransitions');
+			if(elHide && !force_show){
+				switch(direction){
+					case 'top':
+						if (checkCSSTransition) {
+							$this.css({top: (height*(-1))});
+						}else{
+							$this.stop().animate({top: (height*(-1))},speedAction);
+						}
+						break;
+					case 'bottom':
+						if (checkCSSTransition) {
+							$this.css({bottom: (height*(-1))});
+						}else{
+							$this.stop().animate({bottom: (height*(-1))},speedAction);
+						}
+						break;
+					case 'right':
+						if (checkCSSTransition) {
+							$this.css({right: (width*(-1))});
+						}else{
+							$this.stop().animate({right: (width*(-1))},speedAction);
+						}
+						break;
+					case 'left':
+						if (checkCSSTransition) {
+							$this.css({left: (width*(-1))});
+						}else{
+							$this.stop().animate({left: (width*(-1))},speedAction);
+						}
+						break;
+				}
+				$this.data('element-hide',false);
+			}else{
+				switch(direction){
+					case 'top':
+						if (checkCSSTransition) {
+							$this.css({top: 0});
+						}else{
+							$this.stop().animate({top: 0}, speedAction);
+						}
+						break;
+					case 'bottom':
+						if (checkCSSTransition) {
+							$this.css({bottom: 0});
+						}else{
+							$this.stop().animate({bottom: 0}, speedAction);
+						}
+						break;
+					case 'right':
+						if (checkCSSTransition) {
+							$this.css({right: 0});
+						}else{
+							$this.stop().animate({right: 0}, speedAction);
+						}
+						break;
+					case 'left':
+						if (checkCSSTransition) {
+							$this.css({left: 0});
+						}else{
+							$this.stop().animate({left: 0}, speedAction);
+						}
+						break;
+				}
+				$this.data('element-hide',true);
+			}
+		});
 		return this;
 	};
+	
+	$.fn.createSlide = function(idGallery){
+		console.log('settings.seletorGallery',settings.seletorGallery);
+		var $galleryContent = $(idGallery);
+		var idGallery = $galleryContent.id();
+		this.data('galleryLoaded', true);
+		if(settings.optionsBxSlider.onSliderLoad)
+			var defaultOnSliderLoad = settings.optionsBxSlider.onSliderLoad;
+		else
+			var defaultOnSliderLoad = function(){};
+
+		settings.optionsBxSlider.onSliderLoad = function(currentIndex){
+			defaultOnSliderLoad(currentIndex);
+			$slideContent.find('li:eq('+(currentIndex+1)+')').addClass('item-current');
+
+			$galleryContent.find(settings.seletorNext).data('direction-hide', 'right');
+			$galleryContent.find(settings.seletorPrev).data('direction-hide', 'left');
+			$galleryContent.find(settings.seletorHeader).data('direction-hide', 'top');
+			$galleryContent.find(settings.seletorFooter).data('direction-hide', 'bottom');
+			$galleryContent.find(settings.seletorsPanelGalley).data('element-hide', true).addClass(settings.classPanelGallery);
+			$galleryContent.on('click.togglePainels', 'img', function(){
+				$galleryContent.togglePainels('slow', false);
+			});
+
+			if(settings.callback && settings.callback.onSliderLoad)
+				settings.callback.onSliderLoad();
+		}
+
+		gallerySliders[idGallery] = this.bxSlider(settings.optionsBxSlider);
+
+		return gallerySliders[idGallery];
+	};
+
+	var createThumbs = function(){};
 
 })(jQuery)
 
-function togglePainels(speedAction, force_show, $parents, $elPanels){
-	if(!$elPanels){
-		var $elPanels = $parents.find('.PanelGaleria');
-	}
-	$elPanels.each(function(){
-		var $this = $(this);
-		var direction = $this.data('direction-hide');
-		var elHide = $this.data('element-hide');
-		var height = $this.height();
-		var width = $this.width();
-		if(elHide && !force_show){
-			switch(direction){
-				case 'top':
-					$this.stop().animate({top: (height*(-1))},speedAction);
-					break;
-				case 'bottom':
-					$this.stop().animate({bottom: (height*(-1))},speedAction);
-					break;
-				case 'right':
-					$this.stop().animate({right: (width*(-1))},speedAction);
-					break;
-				case 'left':
-					$this.stop().animate({left: (width*(-1))},speedAction);
-					break;
-			}
-			$this.data('element-hide',false);
-		}else{
-			switch(direction){
-				case 'top':
-					$this.stop().animate({top: 0},speedAction);
-					break;
-				case 'bottom':
-					$this.stop().animate({bottom: 0},speedAction);
-					break;
-				case 'right':
-					$this.stop().animate({right: 0},speedAction);
-					break;
-				case 'left':
-					$this.stop().animate({left: 0},speedAction);
-					break;
-			}
-			$this.data('element-hide',true);
-		}
-	});
-}
+var delayExecution = (function(){
+	var timeoutId = 0;
+	return function(callback, ms){
+		clearTimeout (timeoutId);
+		timeoutId = setTimeout(callback, ms);
+	};
+})();
 
-function createSlide($slideContent){
+$(document).ready(function($) {
 	var is_touch_device = 'ontouchstart' in document;
-	return $slideContent.bxSlider({
-		pager: false,
-		adaptiveHeight: true,
-		prevText:'<span class="bullet"></span>',
-		nextText:'<span class="bullet"></span>',
-		touchEnabled: is_touch_device,
-		onSliderLoad: function(currentIndex){
-			$slideContent.find('li:eq('+(currentIndex+1)+')').addClass('item-current');
-		},
-		onSlideBefore:function($slideElement, oldIndex, newIndex){
-			$slideElement.parent().find('li:eq('+(oldIndex+1)+')').removeClass('item-current');
-		},
-		onSlideAfter: function($slideElement, oldIndex, newIndex){
-			$slideElement.addClass('item-current');
-		}
-	});
-}
-
-function Galeriabuild(){
-	var delayExecution = (function(){
-		var timeoutId = 0;
-		return function(callback, ms){
-			clearTimeout (timeoutId);
-			timeoutId = setTimeout(callback, ms);
-		};
-	})();
-
-	var $galleryContent = $(".box-gallery");
-		
-	$galleryContent.each(function(){
-		var $this = $(this),
-			$slideContent = $this.find(".bx-slider");
-		window[$this.attr('id')] = createSlide($slideContent);
-
-		$this.find('img').on('click.togglePainels', function(){
-			togglePainels('slow', false, $this);
-		});
-	});
-	
-	$(".bx-next").data('direction-hide', 'right')
-	$(".bx-prev").data('direction-hide', 'left');
-	$(".headerTitleGallery").data('direction-hide', 'top');
-	$(".infosBottom").data('direction-hide', 'bottom');
-	$(".infosBottom, .headerTitleGallery, .bx-prev, .bx-next").data('element-hide',true).addClass('PanelGaleria');
-
-	$galleryContent.imagesLoaded( function() {});
-	
-	$(".changeGallery a").on('click.changeGallery',function(e){
-		e.preventDefault();
-		var $link = $(this);
-		var idGallery = $link.data('target');
-		var $currentGallery = $('.box-gallery:visible');
-
-		if($currentGallery.attr('id') == idGallery && $currentGallery.size() == 1) return false;
-		
-		var $gallery = $('#'+idGallery);
-		togglePainels('slow', true, $gallery);
-
-		$currentGallery.fadeOut(200,function(){
-			$(this).hide();
-			$(".changeGallery .gallery").removeClass('active');
-			$gallery.fadeIn(200, function(){
-				$link.parent().addClass('active');
-			});
-		});
-
-		return false;
-	})
-
-	$(".changeGallery a").first().trigger('click');
-};
-
-
-
-$(document).ready(function(){
-	Galeriabuild();
-	/*$('.bx-slider li').each(function(){
-		$(this).on({
-			'mouseenter.showPainelsGaleria':function(){
-				$(this).hidePainels();
+	var options = {
+		optionsBxSlider: {
+			pager: false,
+			adaptiveHeight: true,
+			prevText:'<span class="bullet"></span>',
+			nextText:'<span class="bullet"></span>',
+			touchEnabled: is_touch_device,
+			onSliderLoad: function(currentIndex){
+				
 			},
-			'mouseleave.showPainelsGaleria': function (){
-				$(this).showPainels();
+			onSlideBefore:function($slideElement, oldIndex, newIndex){
+				$slideElement.parent().find('li:eq('+(oldIndex+1)+')').removeClass('item-current');
 			},
-			'mousemove.showPainelsGaleria': function() {
+			onSlideAfter: function($slideElement, oldIndex, newIndex){
+				$slideElement.addClass('item-current');
 			}
-		});
-	});
+		}
+	}
 
-	delayExecution(function(){
-		$('.box-gallery.active .bx-slider li:eq(1)').hidePainels();
-	},1000);*/
+	Gallerybuild(options);
 });
